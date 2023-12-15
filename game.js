efa3472a4e17fcd065c6a7cda93032f19795fb12
khobster <152924497+khobster@users.noreply.github.com>
2023-12-15 03:47:52 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,68 +1,98 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
-// Load images
-const travisSpriteSheet = new Image();
-travisSpriteSheet.src = 'travis_sprite_sheet.png';
+// Game variables
+let birdX = 50;
+let birdY = 100;
+let gravity = 0.5;
+let flapPower = 5;
+let score = 0;
+let pipeGap = 100; // Gap between pipes
+let pipeSpeed = 3; // Speed of pipes moving
+let pipes = []; // Array to store pipe objects
 
-const swiftiesUp = new Image();
-swiftiesUp.src = 'swifties_up.png';
+// Images (omitted for brevity, replace with your image paths)
+const birdImg = new Image();
+const pipeImg = new Image();
 
-const swiftiesDown = new Image();
-swiftiesDown.src = 'swifties_down.png';
-
-const backgroundImg = new Image();
-backgroundImg.src = 'background.png';
-
-// Game variables and initialization code...
-let birdX = 50; // Initial horizontal position
-let birdY = 100; // Initial vertical position
-let travisState = 'flyingStraight'; // Possible values: 'flyingUp', 'flyingStraight', 'fallingDown'
-
-// Define frame coordinates for each state (placeholders)
-const frames = {
-    flyingUp: { sourceX: 0, sourceY: 0 },
-    flyingStraight: { sourceX: 64, sourceY: 0 },
-    fallingDown: { sourceX: 0, sourceY: 64 },
-    // Add additional frames if necessary
-};
-
-function gameLoop() {
-    // Clear the canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // Draw the background
-    ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);
-
-    // Determine source coordinates based on Travis's state
-    let frame = frames[travisState];
-
-    // Draw Travis
-    ctx.drawImage(travisSpriteSheet, 64, 64, 64, 64, birdX, birdY, 64, 64);
-
-    // Draw Swifties (up and down)
-    // You'll need to calculate the positions and use drawImage similarly to the bird
-    // ...
-
-    // Rest of game loop logic...
-    // ...
-
-    requestAnimationFrame(gameLoop);
+// Pipe object constructor
+function Pipe(x) {
+  this.x = x;
+  this.topHeight = Math.random() * (canvas.height - pipeGap - 100) + 100; // Random height for top pipe
+  this.bottomHeight = canvas.height - this.topHeight - pipeGap; // Calculate height for bottom pipe
 }
 
-// Event listener for game controls
-document.addEventListener('keydown', (event) => {
-    // Game control logic...
-    if (event.key === 'ArrowUp' || event.key === ' ') {
-        // Logic for Travis moving up
-        travisState = 'flyingUp';
-    } else if (/* condition for Travis moving straight */) {
-        travisState = 'flyingStraight';
-    } else if (/* condition for Travis falling down */) {
-        travisState = 'fallingDown';
+// Game loop
+function gameLoop() {
+  // Update bird position
+  birdY += gravity;
+
+  // Update pipes
+  for (let i = 0; i < pipes.length; i++) {
+    pipes[i].x -= pipeSpeed;
+    if (pipes[i].x < -pipeImg.width) {
+      pipes.shift(); // Remove pipe if it goes off screen
+      score++; // Increase score for passing a pipe
     }
-    // Reset birdY position based on the key pressed
+  }
+
+  // Create new pipe if needed
+  if (pipes.length === 0) {
+    const pipeX = canvas.width;
+    pipes.push(new Pipe(pipeX));
+  }
+
+  // Draw background
+  ctx.fillStyle = '#C0C0C0';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // Draw pipes
+  for (let i = 0; i < pipes.length; i++) {
+    const pipe = pipes[i];
+    ctx.drawImage(pipeImg, pipe.x, 0, pipeImg.width, pipe.topHeight); // Draw top pipe
+    ctx.drawImage(pipeImg, pipe.x, pipe.topHeight + pipeGap, pipeImg.width, pipe.bottomHeight); // Draw bottom pipe
+  }
+
+  // Draw bird
+  ctx.drawImage(birdImg, birdX, birdY);
+
+  // Check collision with pipes
+  for (let i = 0; i < pipes.length; i++) {
+    const pipe = pipes[i];
+    if (birdX + birdImg.width > pipe.x && birdX < pipe.x + pipeImg.width) {
+      if (birdY < pipe.topHeight || birdY > pipe.topHeight + pipeGap) { // Check if bird collides with top or bottom pipe
+        // Collision! Game over
+        alert('Game Over! Your score is: ' + score);
+        pipes = []; // Reset pipes
+        birdY = 100; // Reset bird position
+        score = 0; // Reset score
+        break; // Exit loop
+      }
+    }
+  }
+
+  // Check collision with ground
+  if (birdY + birdImg.height > canvas.height) {
+    // Collision! Game over
+    alert('Game Over! Your score is: ' + score);
+    pipes = []; // Reset pipes
+    birdY = 100; // Reset bird position
+    score = 0; // Reset score
+  }
+
+  // Draw score
+  ctx.fillStyle = '#000';
+  ctx.font = '20px Arial';
+  ctx.fillText(`Score: ${score}`, 10, 20);
+
+  requestAnimationFrame(gameLoop);
+}
+
+// Event listener
+document.addEventListener('keydown', (event) => {
+  if (event.key === ' ') {
+    birdY -= flapPower;
+  }
 });
 
-// Start the game loop
 gameLoop();
