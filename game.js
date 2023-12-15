14ff12a49adb20f62eaa9bdf39ddbf300b335490
khobster <152924497+khobster@users.noreply.github.com>
2023-12-15 03:09:32 -0500

    Update game.js

--- game.js
+++ game.js
@@ -15,17 +15,30 @@ const backgroundImg = new Image();
 backgroundImg.src = 'background.png';
 
 // Game variables and initialization code...
-// ...
+let birdX = 50; // Initial horizontal position
+let birdY = 100; // Initial vertical position
+let travisState = 'flyingStraight'; // Possible values: 'flyingUp', 'flyingStraight', 'fallingDown'
+
+// Define frame coordinates for each state (placeholders)
+const frames = {
+    flyingUp: { sourceX: 0, sourceY: 0 },
+    flyingStraight: { sourceX: 64, sourceY: 0 },
+    fallingDown: { sourceX: 0, sourceY: 64 },
+    // Add additional frames if necessary
+};
 
 function gameLoop() {
     // Clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
+
     // Draw the background
     ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);
 
+    // Determine source coordinates based on Travis's state
+    let frame = frames[travisState];
+
     // Draw Travis
-    ctx.drawImage(travisSpriteSheet, /* sourceX, sourceY, sourceWidth, sourceHeight, */ birdX, birdY, 64, 64);
+    ctx.drawImage(travisSpriteSheet, frame.sourceX, frame.sourceY, 64, 64, birdX, birdY, 64, 64);
 
     // Draw Swifties (up and down)
     // You'll need to calculate the positions and use drawImage similarly to the bird
@@ -40,6 +53,15 @@ function gameLoop() {
 // Event listener for game controls
 document.addEventListener('keydown', (event) => {
     // Game control logic...
+    if (event.key === 'ArrowUp' || event.key === ' ') {
+        // Logic for Travis moving up
+        travisState = 'flyingUp';
+    } else if (/* condition for Travis moving straight */) {
+        travisState = 'flyingStraight';
+    } else if (/* condition for Travis falling down */) {
+        travisState = 'fallingDown';
+    }
+    // Reset birdY position based on the key pressed
 });
 
 // Start the game loop
