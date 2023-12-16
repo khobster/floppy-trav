90035f5c9b7abec558f22b8e1b420cdd9fb7d7c1
khobster <152924497+khobster@users.noreply.github.com>
2023-12-16 17:43:50 -0500

    Update game.js

--- game.js
+++ game.js
@@ -28,64 +28,135 @@ let pipeSpeed = 3;
 let pipes = [];
 let framesSinceLastPipe = 0;
 let pipeInterval = 100;
-let gameRunning = false; // Set to false initially
+let gameRunning = false; // Game should not run until start is pressed
 
 // Images
 const spriteSheet = new Image();
 const pipeImg = new Image();
 
-// Load images
+// Image loading
 let imagesLoaded = 0;
 let totalImages = 2;
 spriteSheet.onload = pipeImg.onload = () => {
-  imagesLoaded++;
-  if (imagesLoaded === totalImages) {
-    document.getElementById('welcomeScreen').style.display = 'block'; // Show welcome screen once images are loaded
-  }
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        // Images are loaded, show the welcome screen
+        document.getElementById('welcomeScreen').style.display = 'block';
+    }
 };
 
-spriteSheet.src = 'travisbird.png';
-pipeImg.src = './pipe.png';
+spriteSheet.src = 'travisbird.png'; // Replace with the path to your sprite sheet
+pipeImg.src = './pipe.png'; // Replace with the path to your pipe image
 
-// Sprite animation frames
+// Sprite animation frames coordinates
 const spriteFrames = [
-  { x: 0, y: 0 },
-  { x: 92, y: 0 },
-  { x: 184, y: 0 }
+  { x: 0, y: 0 },    // Frame 1: Wing down
+  { x: 92, y: 0 },   // Frame 2: Wing mid
+  { x: 184, y: 0 }   // Frame 3: Wing up
 ];
 let currentFrameIndex = 0;
 let frameCount = 0;
 
-// Flap function
+// Function to handle user flap (spacebar, click, or tap)
 function flap() {
   if (gameRunning) {
     flapVelocity = flapPower;
   }
 }
 
-// Event listeners
-canvas.addEventListener('touchstart', flap, false);
-canvas.addEventListener('mousedown', flap, false); 
+// Start the game when the welcome screen is clicked/tapped
+document.getElementById('welcomeScreen').addEventListener('click', startGame);
+document.getElementById('welcomeScreen').addEventListener('touchstart', startGame);
+
+// Event listeners for desktop controls
 document.addEventListener('keydown', function(event) {
   if (event.key === ' ' || event.code === 'Space') {
     flap();
   }
 }, false);
 
-// Function to start the game
 function startGame() {
-  gameRunning = true;
+  // Hide the welcome screen
   document.getElementById('welcomeScreen').style.display = 'none';
+
+  // Start the game loop
+  gameRunning = true;
   gameLoop();
 }
 
-// Add event listeners for the welcome screen
-document.getElementById('welcomeScreen').addEventListener('click', startGame);
-document.getElementById('welcomeScreen').addEventListener('touchstart', startGame);
+// Update the bird's position
+function updateBirdPosition() {
+  birdY -= flapVelocity;
+  flapVelocity *= flapDecay;
+  birdY += gravity;
 
-// Game functions (updateBirdPosition, drawBird, updateFrame, Pipe, drawPipes, updatePipes, checkCollisions) remain unchanged
+  if (birdY < 0) birdY = 0;
+  if (birdY + 64 >= canvas.height) gameOver();
+}
 
-// Game over function
+// Draw the bird
+function drawBird() {
+  const frameX = spriteFrames[currentFrameIndex].x;
+  ctx.drawImage(spriteSheet, frameX, 0, 92, 64, birdX, birdY, 92, 64);
+}
+
+// Update the frame for sprite animation
+function updateFrame() {
+  frameCount++;
+  if (frameCount > 10) {
+    frameCount = 0;
+    currentFrameIndex = (currentFrameIndex + 1) % spriteFrames.length;
+  }
+}
+
+// Pipe constructor
+function Pipe(x) {
+  this.x = x;
+  this.top = Math.random() * (canvas.height / 2);
+  this.bottom = canvas.height - this.top - pipeGap;
+  this.width = pipeImg.width;
+}
+
+// Draw pipes
+function drawPipes() {
+  pipes.forEach(function(pipe) {
+    ctx.drawImage(pipeImg, pipe.x, 0, pipe.width, pipe.top);
+    ctx.drawImage(pipeImg, pipe.x, canvas.height - pipe.bottom, pipe.width, pipe.bottom);
+  });
+}
+
+// Update pipes
+function updatePipes() {
+  if (framesSinceLastPipe === pipeInterval) {
+    pipes.push(new Pipe(canvas.width));
+    framesSinceLastPipe = 0;
+  } else {
+    framesSinceLastPipe++;
+  }
+
+  pipes.forEach(function(pipe, index) {
+    pipe.x -= pipeSpeed;
+    if (pipe.x + pipe.width < 0) {
+      pipes.splice(index, 1);
+    }
+  });
+}
+
+// Check for collisions
+function checkCollisions() {
+  for (let i = 0; i < pipes.length; i++) {
+    let pipe = pipes[i];
+    let hitTopPipe = birdX < pipe.x + pipe.width && birdX + 92 > pipe.x && birdY < pipe.top;
+    let hitBottomPipe = birdX < pipe.x + pipe.width && birdX + 92 > pipe.x && birdY + 64 > canvas.height - pipe.bottom;
+
+    if (hitTopPipe || hitBottomPipe) {
+      gameOver();
+      return;
+    }
+  }
+}
+
+// Handle game over
 function gameOver() {
   gameRunning = false;
   alert('Game Over! Your score is: ' + score);
@@ -94,7 +165,7 @@ function gameOver() {
 
 // Game loop function
 function gameLoop() {
-  if (!gameRunning) return; // Only run if the game has started
+  if (!gameRunning) return;
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   updateBirdPosition();
@@ -104,12 +175,13 @@ function gameLoop() {
   drawPipes();
   checkCollisions();
 
-  // Score display
+  // Score display in the lower left corner
+  ctx.strokeStyle = 'black';
+  ctx.lineWidth = 3;
+  ctx.strokeText('Score: ' + score, 10, canvas.height - 20);
   ctx.fillStyle = 'white';
   ctx.font = '20px Arial';
   ctx.fillText('Score: ' + score, 10, canvas.height - 20);
 
   requestAnimationFrame(gameLoop);
 }
-
-// Game will start after welcome screen interaction
