5039eb941c8858ec4079d424ed23a4ced35d3601
khobster <152924497+khobster@users.noreply.github.com>
2023-12-18 22:12:11 -0500

    Update game.js

--- game.js
+++ game.js
@@ -130,7 +130,7 @@ function drawPipes() {
 }
 
 function updatePipes() {
-    if (score < 6) {
+    if (score < 100) {
         framesSinceLastPipe++;
         if (framesSinceLastPipe >= pipeInterval) {
             pipes.push(new Pipe(canvas.width));
